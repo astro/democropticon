002214bce182a53bb94559564b1c1af6f23a2c9a
Astro <astro@spaceboyz.net>
2015-11-23 02:02:29 +0100

    refactor analysis

--- analysis/tfidf.js
+++ analysis/tfidf.js
@@ -8,6 +8,7 @@ if (!dataDir) {
 }
 
 var tokenHits = {}
+var tokenSpellings = {}
 var maxMatches = 0
 var docCount = 0
 
@@ -19,52 +20,33 @@ docSource(dataDir, function(session) {
         process.exit(1)
     }
 
-    console.log("Preparing tokens")
-    var tokens = Object.keys(tokenHits)
-    tokens.forEach(function(token) {
-        tokenHits[token].tfidf = tfidf(tokenHits[token])
-    })
-    console.log("Sorting...", tokens.length, "tokens")
-    topTokens = tokens.sort(function(token1, token2) {
-        return tokenHits[token2].tfidf - tokenHits[token1].tfidf
-    }).slice(0, 99)
-
-    topTokens.forEach(function(token) {
-        var hit = tokenHits[token]
-        var topSpellings = Object.keys(hit.spellings).sort(function(spelling1, spelling2) {
-            return hit.spellings[spelling2] - hit.spellings[spelling1]
-        }).slice(0, 9)
-        console.log([
-            token,
-            hit.tfidf,
-            hit.matches,
-            hit.docs,
-            topSpellings.join(" ")
-        ].join("\t"))
-    })
+    runSummary()
 })
 
 function handleDoc(doc) {
     var $ = cheerio.load(doc)
     var body = $('body').text()
     var tokens = {}
-    body.split(/[\s,\.;:\?!\/]+/g).forEach(function(token) {
+    body.split(/[\s,\.;:\?!\/ ]+/g).forEach(function(token) {
         token = token.toLowerCase()
         var stemmed = stem(token)
         tokens[stemmed] = true
 
         if (!tokenHits.hasOwnProperty(stemmed)) {
-            tokenHits[stemmed] = { matches: 0, docs: 0, spellings: {} }
+            tokenHits[stemmed] = { matches: 0, docs: 0 }
         }
         tokenHits[stemmed].matches += 1
         if (tokenHits[stemmed].matches > maxMatches) {
             maxMatches = tokenHits[stemmed].matches
         }
 
-        if (tokenHits[stemmed].spellings.hasOwnProperty(token)) {
-            tokenHits[stemmed].spellings[token] = 0
+        if (!tokenSpellings.hasOwnProperty(stemmed)) {
+            tokenSpellings[stemmed] = {}
         }
-        tokenHits[stemmed].spellings[token] += 1
+        if (!tokenSpellings[stemmed].hasOwnProperty(token)) {
+            tokenSpellings[stemmed][token] = 0
+        }
+        tokenSpellings[stemmed][token] += 1
     })
     tokens = Object.keys(tokens)
     // console.log("tokens", tokens)
@@ -78,4 +60,30 @@ function tfidf(hit) {
     var tf = 0.5 + 0.5 * hit.matches / maxMatches
     var idf = Math.log(docCount / hit.docs)
     return tf * idf
+}
+
+function runSummary() {
+    console.log("Preparing tokens")
+    var tokens = Object.keys(tokenHits)
+    tokens.forEach(function(token) {
+        tokenHits[token].tfidf = tfidf(tokenHits[token])
+    })
+    console.log("Sorting...", tokens.length, "tokens")
+    topTokens = tokens.sort(function(token1, token2) {
+        return tokenHits[token2].tfidf - tokenHits[token1].tfidf
+    }).slice(0, 99)
+
+    topTokens.forEach(function(token) {
+        var topSpellings = Object.keys(tokenSpellings[token]).sort(function(spelling1, spelling2) {
+            return tokenSpellings[spelling2] - tokenSpellings[spelling1]
+        }).slice(0, 9)
+        var hit = tokenHits[token]
+        console.log([
+            token,
+            hit.tfidf,
+            hit.matches,
+            hit.docs,
+            topSpellings.join(" ")
+        ].join("\t"))
+    })
 }
